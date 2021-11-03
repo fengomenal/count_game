ac47411c78ebc0da87e6abbe9cd43bc7aeb514e7
fengomenal <ubuntu@ip-172-31-25-145.ec2.internal>
2021-11-03 02:40:41 +0000

    count js

--- count.js
+++ count.js
@@ -16,12 +16,41 @@ let signs = [1, -1];
 async function count() {
   while (true) {
     await pause(Math.floor(Math.random() * (8000 - 4000)) + 4000);
+    if (go) {
+      let sign = signs[Math.floor((Math.random() * signs.length))];
+      let number = Math.floor(Math.random() * (maxNum - minNum) + minNum);
+      nextNumber = sign * number;
+      totalCount += nextNumber;
+      round += 1;
+      speech.text = nextNumber > 0 ? `plus ${nextNumber}` : nextNumber;
+      window.speechSynthesis.speak(speech);
+    }
   }
-}
+};
 
 function pause(duration) {
   return new Promise(resolve => setTimeout(() => {
     resolve();
   }, duration));
-}
+};
+
+function setGo(bool) {
+  go = bool;
+  if (go) {
+    $("#status").replaceWith("<span id=\"status\">&#128994;</span>");
+  } else {
+    $("#status").replaceWith("<span id=\"status\">&#128308;</span>");
+  }
+};
+
+function showCurrentValue() {
+  $("#totalCount").text(`Count: ${totalCount}`);
+  $("#round").text(`Round: ${round}`);
+};
+
+$(document).ready(() => {
+  showCurrentValue();
+  setGo(false);
+  count();
+});
 
